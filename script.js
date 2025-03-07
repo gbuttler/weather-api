52f3952570485180f5f67c924f41edc16267f694
Grace Buttler <grace.buttler@gmail.com>
2025-03-07 11:57:31 +0000

    create function with variable that allows location to be changed

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// get the data to show up
+// get the data to show up using a set variable
+let placeName = "Cardiff";
 
-fetch(
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Cardiff?unitGroup=us&key=LLJRM2WGCRY8YRHE89992BHH8&contentType=json",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log(response);
-    console.log(response.description);
-  });
+function getWeather(placeName) {
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${placeName}?unitGroup=metric&key=LLJRM2WGCRY8YRHE89992BHH8&contentType=json`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      console.log(response.resolvedAddress);
+    });
+}
+
+getWeather("Swansea");
